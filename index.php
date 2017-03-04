54f293f0db24490cda0b6c0bca079ef4de556ed1
Mark Mwaura <markkmwaura@gmail.com>
2017-03-04 15:59:22 +0300

    more custom

--- index.php
+++ index.php
@@ -226,11 +226,11 @@
                 </div>
                 <div class="col-lg-4 col-lg-offset-2 text-center">
                     <i class="fa fa-phone fa-3x sr-contact"></i>
-                    <p>123-456-6789</p>
+                    <p>071#######</p>
                 </div>
                 <div class="col-lg-4 text-center">
                     <i class="fa fa-envelope-o fa-3x sr-contact"></i>
-                    <p><a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a></p>
+                    <p><a href="mailto:your-email@your-domain.com">markkmwaura@gmail.com</a></p>
                 </div>
             </div>
         </div>
